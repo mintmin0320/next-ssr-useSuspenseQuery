017bdf2db71a3b159bf51fa3aca859d201df53ad
mintmin <mintmin0320@gmail.com>
2025-02-02 12:48:57 +0900

    style: router 구조 분해 할당 적용

--- src/app/_components/FirstComponent.tsx
+++ src/app/_components/FirstComponent.tsx
@@ -5,16 +5,16 @@ import { useRouter } from 'next/navigation'
 import { useCookieContext } from '../_test/useCookieContext'
 
 const FirstComponent = () => {
-  const router = useRouter()
+  const { push } = useRouter()
   const { setToken } = useCookieContext()
 
   const onLogin = () => {
-    const tokenValue = '167|QwXRt7T25VLdj54ahChm1q55IPNwcs1scwC1nPNA70e5dfe9';
+    const tokenValue = '194|GSNxPjArX2ZGjemgSVdQEizz6TJAocpxcvQjEHGR597e752a';
     
     setCookie('token', tokenValue);
     setToken(tokenValue)
 
-    router.push('/item');
+    push('/item');
   };
 
   return (
